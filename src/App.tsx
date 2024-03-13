371ed59c03f194c791b6972d1d5e55512a62298a
Jimmy García <jimmyegc@gmail.com>
2024-03-12 21:58:25 -0600

    fix 03122024

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,32 @@ import { Input } from "./components/ui/Input";
 import { Label } from "./components/ui/Label"; */
 import { Label, Input, Button } from "./components/ui";
 
+import { useState } from "react";
+
+const useMyHook = (initialState = false) => {
+  const [active, setActive] = useState(initialState);
+  const handleToggle = () => setActive(!active);
+  const handleFalse = () => setActive(false);
+  const handleTrue = () => setActive(true);
+
+  return {
+    active,
+    handleToggle,
+  };
+};
+
 function App() {
+  const { active, handleToggle } = useMyHook();
   return (
     <>
-      <h1>App</h1>
+      <h2>Custom Hooks</h2>
+      <div className="container mx-auto bg-slate-400">
+        <Button onClick={handleToggle}>toggle</Button>
+        {active.toString()}
+        <ShowInfo />
+      </div>
+
+      <h2>UI</h2>
       <form action="" className="container mx-auto">
         <div className="mt-2">
           <Label htmlFor="email">Correo Electrónico</Label>
@@ -33,5 +55,19 @@ function App() {
     </>
   );
 }
+
+export const ShowInfo = () => {
+  const { active, handleToggle } = useMyHook(false);
+  return (
+    <>
+      <Button onClick={handleToggle}>Show/Hide</Button>
+      {active && (
+        <div className="bg-lime-300 h-28">
+          <h1>User Info</h1>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default App;
